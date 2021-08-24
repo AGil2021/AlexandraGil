2ca926f2a21107ca0fe3795550514e4b8b4ff833
Alexandra Gil <ag66152@uga.edu>
2021-08-23 22:19:50 -0400

    Introduction

---
+++ aboutme.R
@@ -0,0 +1,45 @@
+title: "About me"
+output: 
+  html_document:
+  toc: FALSE
+
+## Hi, My name is Alexandra "Alex"
+
+```{r, echo=FALSE, out.width='25%', fig.align='center'}
+knitr::include_graphics ('Portfolio-picture.jpg')
+```
+> 
+  > ### My Background
+  > 
+  > I am a second year MPH student, my concentration is in Biostatistics. 
+  I always think that the knowledge of the data will give you decision power, 
+  that the main reason that I decided to come back to school after so many years. 
+  I am originally from Colombia, I got my bachelor degree as a Food Engineer back in 2011.
+  I decided to move to the Unites States in 2015 and just until 2020 in the middle of the 
+  COVID-19 Pandemic I started this journey. It amazing how you can find data about so 
+  many topics and you can transforme the data to create big impact over the entire population.
+  
+  ### Experience
+  I work at Gwinnett Health Department as a Health Inspector, 
+  I have been working as a part of the COVID-19 response team, 
+  during the last year and halft. My goal is be fully involved in the data analysis
+  for infectious diseases.
+  
+  ### Course Goals
+  Througth this MPH journey I have been using STATA, SAS and R. I have some personal preference ,
+  however, I really enjoy the facts that you can find tons of good resourses about R just using Google. Making
+  our life a little be easy. With MADA I have so many exceptations, such as:
+    - Improve my data analysis, specifically in infectious diseases.
+    - Get familiar with clean data 
+    - Process data in R to get usable outcomes
+    - Learn about machine learning
+  
+  ### Hobbies and Fun Facts
+  - Deep sea fishing has been a familiar activity that started as a curiosity and now is 
+    part of our summers.
+    
+  - As my first language is Spanish I really enjoy doing activities to improve my English 
+    proficiency such as, listen podcast or reading.
+  
+  
+  
